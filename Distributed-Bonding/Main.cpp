300f29561c94ae136e9c5aad7206c344645060ef
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-03-01 11:40:57 +0800

    Removed global and feed data to constructor instead

--- Distributed-Bonding/Main.cpp
+++ Distributed-Bonding/Main.cpp
@@ -2,10 +2,19 @@
 #include "Client.h"
 #include "Globals.h"
 
+// Type of Machine (0 - Client, 1 - Server)
+constexpr int MACHINE_TYPE = 0;
+constexpr int MOLECULE_TYPE = 0; // (For client, 0 - H, 1 - O)
+
+// Master Server
+std::string MASTER_HOST = "192.168.1.5";
+int MASTER_PORT = 5000;
+
 
 int main() {
 	if (MACHINE_TYPE == 0) {
 		Client client(MOLECULE_TYPE);
+		client.init(MASTER_HOST, MASTER_PORT);
 	} else {
 		//Server server;
 		//server.run();
