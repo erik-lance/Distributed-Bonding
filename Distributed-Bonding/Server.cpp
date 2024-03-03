a0048e146e214e289a2c92e4d9995a23c29cc87e
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-03-03 14:24:37 +0800

    Add debug statements to Server constructor

--- Distributed-Bonding/Server.cpp
+++ Distributed-Bonding/Server.cpp
@@ -44,8 +44,11 @@ Server::Server(std::string hostname, int portNum)
 		exit(1);
 	}
 
+	std::cout << "Server waiting for 2 connections" << std::endl;
 	// Accept two connections
 	connected_clients.push_back(accept(m_socket, NULL, NULL));
+
+	std::cout << "Server waiting for 1 more connection" << std::endl;
 	connected_clients.push_back(accept(m_socket, NULL, NULL));
 }
 
