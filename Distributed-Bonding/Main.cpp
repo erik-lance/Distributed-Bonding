e7d2ea02cd1651a8c52f26cf08c58e5005de880b
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-03-03 14:08:31 +0800

    Refactor server initialization in main function

--- Distributed-Bonding/Main.cpp
+++ Distributed-Bonding/Main.cpp
@@ -7,14 +7,6 @@
 std::string MASTER_HOST = "127:0.0.1";
 int MASTER_PORT = 5000;
 
-// Hydrogen Server
-std::string HYDROGEN_HOST = "127.0.0.1";
-int HYDROGEN_PORT = 5001;
-
-// Oxygen Server
-std::string OXYGEN_HOST = "127:0.0.1";
-int OXYGEN_PORT = 5002;
-
 int main()
 {
 	int machineType;
@@ -33,14 +25,8 @@ int main()
 		std::cout << "Enter the molecule type (0 - H, 1 - O): ";
 		std::cin >> moleculeType;
 		Client client(moleculeType);
-		if (moleculeType == 0)
-		{
-			client.init(HYDROGEN_HOST, HYDROGEN_PORT);
-		}
-		else
-		{
-			client.init(OXYGEN_HOST, OXYGEN_PORT);
-		}
+		client.init(MASTER_HOST, MASTER_PORT);
+		client.run();
 	}
 
 	return 0;
